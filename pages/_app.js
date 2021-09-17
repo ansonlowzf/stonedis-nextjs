bb6985b0c61573da43f44ca7b9c1eff38ed68b54
Anson Low Z.F <zhanfaye@gmail.com>
2021-09-17 12:04:15 +0800

    add google analytics follow nextjs example

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
@@ -7,7 +9,8 @@ import { CacheProvider } from "@emotion/react";
 import createEmotionCache from "../styles/createEmotionCache";
 
 import theme from "../styles/theme";
-import "../styles/globals.css";
+
+import * as gtag from "../element/Gtag";
 
 import Header from "@components/Header";
 import Footer from "@components/Footer";
@@ -18,20 +21,60 @@ const clientSideEmotionCache = createEmotionCache();
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Stone Distributor Malaysia</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <Header />
-        <CssBaseline />
-        <Component {...pageProps} />
-        <Footer />
-      </ThemeProvider>
-    </CacheProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="ga-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Stone Distributor Malaysia</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <Header />
+          <CssBaseline />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Component {...pageProps} />
+          <Footer />
+        </ThemeProvider>
+      </CacheProvider>
+      <style jsx global>
+        {`
+          .active {
+            border-bottom: 1px solid white;
+          }
+        `}
+      </style>
+    </>
   );
 }
 
